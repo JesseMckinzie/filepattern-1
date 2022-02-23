250db3786cd53277bad306ca7ff7aa441f4385a4
JesseMcKinzie <jessemckinzie145@gmail.com>
2022-02-23 14:48:58 +0000

    Update

--- docs/source/Reference/Classes.rst
+++ docs/source/Reference/Classes.rst
@@ -5,5 +5,5 @@ Classes
 .. autosummary::
    :toctree: stubs
 
-   filepattern2.FilePattern2.FilePattern
+   filepattern2.FilePattern
    
--- src/filepattern2/__init__.py
+++ src/filepattern2/__init__.py
@@ -1,3 +1,5 @@
 from __future__ import absolute_import, unicode_literals
 
 from .FilePattern2 import FilePattern
+
+__all__ = ["FilePattern"]
