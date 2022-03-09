9a9626d4ba813c5d007f093783427c7af0b7e500
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2022-03-09 13:21:09 -0500

    Mac updates

--- tests/vector_pattern.py
+++ tests/vector_pattern.py
@@ -1,12 +1,13 @@
 import filepattern2 as fp
 import pprint
 
-path = '/home/ec2-user/Dev/FilePattern/tests/test_data/stitching-vector-61b3a82d6fa1f65bf8cc2448-img-global-positions-1.txt'
+path = 'tests/test_data/stitching-vector-61b3a82d6fa1f65bf8cc2448-img-global-positions-1.txt'
 
 pattern = 'x{x:dd}_y{y:dd}_wx{wx:d}_wy{wy:d}_c1.ome.tif'
 
-files = fp.FilePattern(path, pattern)
+files = fp.FilePattern(path, pattern, "1 GB")
 
-matching = files.get_matching(y=[1])
+for file in files():
+    pprint.pprint(file)
 
-pprint.pprint(matching)
+
