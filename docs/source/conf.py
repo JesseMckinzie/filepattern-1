5882b10eb2cc66c9097d4a206d85e5818dbbfdeb
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2022-03-10 14:19:32 -0500

    Update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -85,7 +85,7 @@ autodoc_default_options = {
 autosummary_generate = True
 
 # Set the master doc
-master_doc = 'main'
+master_doc = 'Examples'
 
 
 # -- Options for HTML output -------------------------------------------------
