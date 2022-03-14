d41c913fe473db335c5ed8391753a5e1c8a1f5af
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2022-03-14 13:36:42 -0400

    Updates to pytest refactor

--- tests/test_inferpattern.py
+++ tests/test_inferpattern.py
@@ -1,62 +1,54 @@
-import unittest, json
+import json, pytest
 from pathlib import Path
 import filepattern2 as fp
 
-"""
-NOTE: Run the file "generate_data.py" in /tests and in "examples" before running this file.
-"""
-
-class InferenceTest(unittest.TestCase):
-    """Verify VERSION is correct """
-
-    json_path = Path(__file__).parent.parent.joinpath("plugin.json")
+json_path = Path(__file__).parent.parent.joinpath("plugin.json")
+with open(Path(__file__).with_name('test_infer_pattern_data.json'),'r') as fr:        
+    data = json.load(fr)
     
-    def setUp(self):
-        
-        with open(Path(__file__).with_name('test_infer_pattern_data.json'),'r') as fr:
-            
-            self.data = json.load(fr)
+class TestInference():
+    """Verify VERSION is correct """
             
     def test_numeric_fixed_width(self):
         
-        pattern = fp.infer_pattern(files=self.data['robot'])
+        pattern = fp.infer_pattern(files=data['robot'])
 
         assert pattern =='00{r:d}0{t:dd}-{c:d}-00100100{z:d}.tif'
 
     def test_alphanumeric_fixed_width(self):
         
-        pattern = fp.infer_pattern(files=self.data['brain'])
+        pattern = fp.infer_pattern(files=data['brain'])
 
         assert pattern == 'S1_R{r:d}_C1-C11_A1_y0{t:dd}_x0{c:dd}_c0{z:dd}.ome.tif'
         
     def test_alphanumeric_variable_width(self):
         
-        pattern = fp.infer_pattern(files=self.data['variable'])
+        pattern = fp.infer_pattern(files=data['variable'])
 
         assert pattern == 'S1_R{r:d}_C1-C11_A1_y{t:d+}_x{c:d+}_c{z:d+}.ome.tif'
 
     def test_alphanumeric_channel_variable_width(self):
         
-        pattern = fp.infer_pattern(files=self.data['channel'])
+        pattern = fp.infer_pattern(files=data['channel'])
 
         assert pattern =='img_r00{r:d}_c00{t:d}_{c:c+}.tif'
     
     def test_variable_naming(self):
-        pattern = fp.infer_pattern(files=self.data['channel'], variables='xyz')
+        pattern = fp.infer_pattern(files=data['channel'], variables='xyz')
         
         assert pattern == 'img_r00{x:d}_c00{y:d}_{z:c+}.tif'
 
     def test_alphanumeric_both_variable_width(self):
 
-        pattern = fp.infer_pattern(files=self.data['both'])
+        pattern = fp.infer_pattern(files=data['both'])
 
         assert pattern == '{r:cccccc}_{t:d+}_{c:c+}.ome.tif'
     
     def test_invalid_input(self):
-        with self.assertRaises(RuntimeError):
-            pattern = fp.infer_pattern(files=self.data['invalid'])
+        with pytest.raises(RuntimeError):
+            pattern = fp.infer_pattern(files=data['invalid'])
 
-class ExternalInferenceTest(unittest.TestCase):
+class TestExternalInference():
 
     path = 'test_data/data'
     def test_fp_single_block(self):
@@ -92,3 +84,4 @@ class ExternalInferenceTest(unittest.TestCase):
         pattern = fp.infer_pattern(path=path, block_size='1 KB')
         
         assert pattern == 'x{r:dd}_y{t:dd}_wx{c:d}_wy{z:d}_c1.ome.tif'
+        
