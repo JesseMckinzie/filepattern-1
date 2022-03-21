d268c35666c9591b78b7118219ba8f057dba8552
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2022-03-21 10:40:24 -0400

    Update install_win_filepattern2.bat

--- install_win_filepattern2.bat
+++ install_win_filepattern2.bat
@@ -4,6 +4,7 @@ python -m pip install delvewheel
 python setup.py bdist_wheel -d dist
 pushd dist
 for %%j in (*.whl) do (
+  delvewheel show %%j  
   delvewheel repair %%j
   pip install wheelhouse\%%j 
   ) 
