da7ab244db571a28d35fa75aae27cb8d5ad5feed
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2023-10-20 06:49:45 -0600

    Update tests

---
+++ tests/java/TestData.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+
+import filepattern.java.*;
+
+public class TestData {
+    
+    private int max_r = 3;
+    private int max_c = 3;
+    private ArrayList<String> channels;
+
+
+    public TestData() {
+        channels.add("DAPI", "TXREAD", "GFP");
+    }
+
+    public void generateData() {
+        
+    }
+
+    public void generateResults() {
+
+
+    }
+}
---
+++ tests/java/TestFilePattern.java
@@ -0,0 +1,24 @@
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestFilePattern {
+    
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+    
+    @Test
+    public void testFilePattern() {
+        
+    }
+
+}
