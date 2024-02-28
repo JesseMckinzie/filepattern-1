5428281ed100938d2169c1b8280e97a86ba993cf
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2024-02-28 10:59:35 -0700

    Update install_prereq_linux.sh

--- ci-utils/install_prereq_linux.sh
+++ ci-utils/install_prereq_linux.sh
@@ -14,9 +14,9 @@ fi
 mkdir -p "$LOCAL_INSTALL_DIR"
 mkdir -p "$LOCAL_INSTALL_DIR"/include
 
-curl -L https://github.com/pybind/pybind11/archive/refs/tags/v2.10.0.zip -o v2.10.0.zip
-unzip v2.10.0.zip
-cd pybind11-2.10.0
+curl -L https://github.com/pybind/pybind11/archive/refs/tags/v2.9.2.zip -o v2.9.2.zip
+unzip v2.9.2.zip
+cd pybind11-2.9.2
 mkdir build_man
 cd build_man/
 cmake -DCMAKE_INSTALL_PREFIX=../../"$LOCAL_INSTALL_DIR"/  -DPYBIND11_TEST=OFF ..
