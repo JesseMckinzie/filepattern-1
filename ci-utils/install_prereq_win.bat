27a2d6b5e15292d48e9b6d2ae89fc3901c6000d7
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2024-02-28 09:55:40 -0700

    Update install_prereq_win.bat

--- ci-utils/install_prereq_win.bat
+++ ci-utils/install_prereq_win.bat
@@ -1,9 +1,9 @@
 mkdir local_install
 mkdir local_install\include
 
-curl -L https://github.com/pybind/pybind11/archive/refs/tags/v2.11.1.zip -o v2.11.1.zip
-unzip v2.11.1.zip
-pushd pybind11-2.11.1
+curl -L https://github.com/pybind/pybind11/archive/refs/tags/v2.10.0.zip -o v2.10.0.zip
+unzip v2.10.0.zip
+pushd pybind11-2.10.0
 mkdir build_man
 pushd build_man
 cmake -DCMAKE_INSTALL_PREFIX=../../local_install/  -DPYBIND11_TEST=OFF ..
